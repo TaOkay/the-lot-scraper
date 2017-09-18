c652dc16eb9eceb49a0efd4768616846b569886c
Darin Fontes <darin@Darins-MacBook-Pro.local>
2017-09-17 20:07:30 -0700

    Added crude retry system

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 var page = new WebPage();
 var url = '';
+var retryCount = 100;
 
 // TODO - Calculate url based on current date
 url = 'https://offthegrid.com/event/the-lot/2017-9-20-11am';
 
 page.open(url, function(status) {
   if (status === 'success') {
-    var vendors = page.evaluate(function() {
-      return $("li.grid-item-card header").map(function() {
-        // TODO  - Add in the category in parens
-        return '* ' + this.innerText;
-      }).toArray().join('\n');
-    });
+    var vendors;
+    var count = 0;
+    while (!vendors) {
+      vendors = page.evaluate(function() {
+        return $("li.grid-item-card div.content-wrap").map(function() {
+          // TODO  - Add in the category in parens
+          return '* ' + this.innerText;
+        }).toArray().join('\n');
+      });
+    
+      count++;
+      if (count > retryCount && !vendors) {
+        vendors = "Couldn't load anything after " + retryCount + " tries.";
+      }
+    };
 
     console.log('Food trucks at The Lot today:');
     console.log(vendors);
   } else {
     console.log('Something bad happened: ' + status);
-  }
+  };
   phantom.exit();
 });
 
