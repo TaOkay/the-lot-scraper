bda73bda4d4de00c96ac98932b97aa646f5cc0ba
Darin Fontes <darin.fontes@infor.com>
2017-09-21 14:31:24 -0700

    Merge index2 to index

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ var PAGE = new WebPage();
 var URL = '';
 var WAIT_TIME_MS = 2000;
 
-// TODO - Calculate URL based on current date
-URL = 'https://offthegrid.com/event/the-lot/2017-9-20-11am';
+var date = new Date();
+URL = 'https://offthegrid.com/event/the-lot/' +
+  date.getFullYear() + '-' +
+  (date.getMonth() + 1) + '-' +
+  date.getDate() + '-11am';
+
+console.log('Scraping: ' + URL);
 
 PAGE.open(URL, function(status) {
   if (status === 'success') {
     // Wait a little bit to let the page load fully
-    setTimeout(function(){}, WAIT_TIME_MS);
-
-    var vendors = PAGE.evaluate(function() {
-      return $("li.grid-item-card div.content-wrap").map(function() {
-        var name = $("h3", this)[0].innerText;
-        var cat = $("span", this)[0].innerText;
-        return '* ' + name + " (" + cat + ")";
-      }).toArray().join('\n');
-    });
-
-    console.log('Food trucks at The Lot today:');
-    console.log(vendors);
+    setTimeout(function() { 
+      scrape();
+      phantom.exit();
+    }, WAIT_TIME_MS);
   } else {
     console.log('Something bad happened: ' + status);
+    phantom.exit();
   };
-  phantom.exit();
 });
+
+function scrape() {
+  var vendors = PAGE.evaluate(function() {
+    return $("li.grid-item-card div.content-wrap").map(function() {
+      var name = $("h3", this)[0].innerText;
+      var cat = $("span", this)[0].innerText;
+      return '* ' + name + " (" + cat + ")";
+    }).toArray().join('\n');
+  });
+
+  console.log('Food trucks at The Lot today:');
+  console.log(vendors);
+};
 
 // PAGE.onConsoleMessage = function(msg, line, source) {
 //   console.log('console> ' + msg);
